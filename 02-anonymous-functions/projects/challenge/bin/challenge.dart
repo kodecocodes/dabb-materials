161aecfb2f64abdfa85d373f528d7834a641ee6a
suragch <suragch@suragch.dev>
2022-07-11 15:38:45 -0700

    gitignore fixed

---
+++ 02-anonymous-functions/projects/challenge/bin/challenge.dart
@@ -0,0 +1,123 @@
+// Copyright (c) 2022 Razeware LLC
+// For full license & permission details, see LICENSE.
+
+void main() {
+  higherOrderFunctionsExercise();
+  callbackFunctionsExercise();
+
+  challenge1();
+  challenge2();
+}
+
+/// Exercise: Higher-Order Functions
+///
+/// Given the following exam scores:
+///
+/// ```dart
+/// final scores = [89, 77, 46, 93, 82, 67, 32, 88];
+/// ```
+///
+/// 1. Use `sort` to order the grades from highest to lowest.
+/// 2. Use `where` to find all the B grades, that is, all the scores
+///    between `80` and `90`.
+void higherOrderFunctionsExercise() {
+  final scores = [89, 77, 46, 93, 82, 67, 32, 88];
+  scores.sort((a, b) => b.compareTo(a));
+  print('Hightest to lowest: $scores');
+  final highest = scores.first;
+  final lowest = scores.last;
+  print(highest);
+  print(lowest);
+
+  final bGrades = scores.where((score) => score >= 80 && score < 90);
+  print(bGrades);
+}
+
+/// Exercise: Callback Functions
+void callbackFunctionsExercise() {
+  /// 5. In `main`, create an instance of `Surface` and pass in an anonymous
+  ///    function that prints the x,y coordinates.
+  final surface = Surface((x, y) => print('($x, $y)'));
+
+  /// 6. Still in `main`, call `touch` where x is `202.3` and y is `134.0`.
+  surface.touch(202.333, 134.0);
+}
+
+/// 3. Make a type alias named `TouchHandler` for the callback function.
+typedef TouchHandler = void Function(double x, double y);
+
+/// 1. Create a class named `Surface`.
+class Surface {
+  Surface(this.onTouch);
+
+  /// 2. Give the class a property named `onTouch`, which is a
+  ///    callback function that provides x,y coordinates but returns nothing.
+  final TouchHandler onTouch;
+
+  /// 4. In `Surface`, create a method named `touch`, which takes x,y
+  ///    coordinates and then internally calls `onTouch`.
+  void touch(double x, double y) => onTouch(x, y);
+}
+
+/// Challenge 1: Animalsss
+///
+/// Given the map below:
+///
+/// ```
+/// final animals = {
+///   'sheep': 99,
+///   'goats': 32,
+///   'snakes': 7,
+///   'lions': 80,
+///   'seals': 18,
+/// };
+/// ```
+///
+/// User higher-order functions to find the total number of animals whose
+/// names begin with "s". That is, how many sheep, snakes and seals are there?
+void challenge1() {
+  final animals = {
+    'sheep': 99,
+    'goats': 32,
+    'snakes': 7,
+    'lions': 80,
+    'seals': 18,
+  };
+
+  final totalBeginningWithS = animals.keys
+      .where((key) => key.startsWith('s'))
+      .fold<int>(0, (int sum, key) => sum + (animals[key] ?? 0));
+  print(totalBeginningWithS);
+}
+
+/// Challenge 2: Can you repeat that?
+///
+/// Write a function named `repeatTask` that looks like this:
+///
+/// ```
+/// int repeatTask(int times, int input, Function task)
+/// ```
+///
+/// It repeats a given `task` on `input` for `times` number of times.
+///
+/// Pass an anonymous function to `repeatTask` to square the input
+/// of 2 four times. Confirm that you get the result 65536, since
+/// 2 squared is 4,
+/// 4 squared is 16,
+/// 16 squared is 256, and
+/// 256 squared is 65536.
+void challenge2() {
+  int repeatTask(int times, int input, Function task) {
+    int result = task(input);
+    for (var i = 1; i < times; i++) {
+      result = task(result);
+    }
+    return result;
+  }
+
+  final result = repeatTask(4, 2, (num input) {
+    return input * input;
+  });
+
+  print(result);
+}
---
+++ 02-anonymous-functions/projects/final/bin/final.dart
@@ -0,0 +1,299 @@
+// Copyright (c) 2022 Razeware LLC
+// For full license & permission details, see LICENSE.
+
+void main() {
+  assigningFunctionsToVariables();
+  passingFunctionsToFunctions();
+  returningFunctionsFromFunctions();
+  iteratingOverAList();
+  iteratingOverAMap();
+  mapping();
+  filtering();
+  reducing();
+  folding();
+  sorting();
+  combiningHigherOrderMethods();
+  voidCallback();
+  valueSetterCallback();
+  valueGetterCallback();
+  tearOffs();
+  typedefs();
+  closures();
+}
+
+void assigningFunctionsToVariables() {
+  int number = 4;
+  String greeting = 'hello';
+  bool isHungry = true;
+
+  Function multiply = (int a, int b) {
+    return a * b;
+  };
+}
+
+void passingFunctionsToFunctions() {
+  void namedFunction(Function anonymousFunction) {
+    // function body
+  }
+}
+
+void returningFunctionsFromFunctions() {
+  Function namedFunction() {
+    return () => print('hello');
+  }
+}
+
+void iteratingOverAList() {
+  const numbers = [1, 2, 3];
+
+  numbers.forEach((int number) {
+    print(3 * number);
+  });
+
+  numbers.forEach((number) {
+    print(3 * number);
+  });
+
+  numbers.forEach((number) => print(3 * number));
+
+  for (final number in numbers) {
+    print(3 * number);
+  }
+
+  final triple = (int x) => print(3 * x);
+  numbers.forEach(triple);
+}
+
+void iteratingOverAMap() {
+  final flowerColor = {
+    'roses': 'red',
+    'violets': 'blue',
+  };
+
+  flowerColor.forEach((flower, color) {
+    print('$flower are $color');
+  });
+
+  print('i \u2764 Dart');
+  print('and so do you');
+}
+
+void mapping() {
+  const numbers = [2, 4, 6, 8, 10, 12];
+
+  final looped = <int>[];
+  for (final x in numbers) {
+    looped.add(x * x);
+  }
+  print(looped);
+
+  final mapped = numbers.map((x) => x * x);
+  print(mapped);
+  print(mapped.toList());
+}
+
+void filtering() {
+  final myList = [1, 2, 3, 4, 5, 6];
+  final odds = myList.where((element) => element.isOdd);
+  print(odds);
+}
+
+void reducing() {
+  const evens = [2, 4, 6, 8, 10, 12];
+  final total = evens.reduce((sum, element) => sum + element);
+  print(total);
+
+  // final emptyList = <int>[];
+  // final result = emptyList.reduce((sum, x) => sum + x);
+}
+
+void folding() {
+  const evens = [2, 4, 6, 8, 10, 12];
+  final total = evens.fold<int>(
+    0,
+    (sum, element) => sum + element,
+  );
+  print(total);
+
+  final emptyList = <int>[];
+  final result = emptyList.fold<int>(
+    0,
+    (sum, element) => sum + element,
+  );
+  print(result);
+}
+
+void sorting() {
+  final desserts = ['cookies', 'pie', 'donuts', 'brownies'];
+
+  desserts.sort();
+  print(desserts);
+
+  desserts.sort((d1, d2) => d1.length.compareTo(d2.length));
+  print(desserts);
+}
+
+void combiningHigherOrderMethods() {
+  declarative();
+  imperative();
+}
+
+void declarative() {
+  const desserts = ['cake', 'pie', 'donuts', 'brownies'];
+  final bigTallDesserts = desserts
+      .where((dessert) => dessert.length > 5)
+      .map((dessert) => dessert.toUpperCase())
+      .toList();
+  print(bigTallDesserts);
+}
+
+void imperative() {
+  const desserts = ['cake', 'pie', 'donuts', 'brownies'];
+  final bigTallDesserts = <String>[];
+  for (final item in desserts) {
+    if (item.length > 5) {
+      final upperCase = item.toUpperCase();
+      bigTallDesserts.add(upperCase);
+    }
+  }
+  print(bigTallDesserts);
+}
+
+class Button {
+  Button({
+    required this.title,
+    required this.onPressed,
+  });
+
+  final String title;
+  // final Function onPressed;
+  final void Function() onPressed;
+}
+
+void voidCallback() {
+  final myButton = Button(
+    title: 'Click me!',
+    onPressed: () {
+      print('Clicked');
+    },
+  );
+  myButton.onPressed();
+  myButton.onPressed.call();
+
+  // final anotherButton = Button(
+  //   title: 'Click me, too!',
+  //   onPressed: (int apple) {
+  //     print('Clicked');
+  //     return 42;
+  //   },
+  // );
+}
+
+class MyWidget {
+  MyWidget({
+    required this.onTouch,
+  });
+
+  final void Function(double xPosition) onTouch;
+}
+
+void valueSetterCallback() {
+  final myWidget = MyWidget(
+    onTouch: (x) => print(x),
+  );
+  myWidget.onTouch(3.14);
+}
+
+class AnotherWidget {
+  AnotherWidget({
+    this.timeStamp,
+  });
+
+  final String Function()? timeStamp;
+}
+
+void valueGetterCallback() {
+  final myWidget = AnotherWidget(
+    timeStamp: () => DateTime.now().toIso8601String(),
+  );
+
+  final timeStamp = myWidget.timeStamp?.call();
+  print(timeStamp);
+}
+
+class StateManager {
+  int _counter = 0;
+
+  void handleButtonClick() {
+    _counter++;
+  }
+}
+
+void tearOffs() {
+  final manager = StateManager();
+
+  final myButton = Button(
+    title: 'Click me!',
+    onPressed: manager.handleButtonClick,
+  );
+  myButton.onPressed();
+
+  const cities = ['Istanbul', 'Ankara', 'Izmir', 'Bursa', 'Antalya'];
+  cities.forEach((city) => print(city));
+
+  cities.forEach(print);
+}
+
+typedef MapBuilder = Map<String, int> Function(List<int>);
+typedef ZipCode = int;
+
+class Gizmo {
+  Gizmo({
+    required this.builder,
+  });
+
+  final MapBuilder builder;
+}
+
+void typedefs() {
+  final gizmo = Gizmo(builder: (list) => {'hi': list.first});
+
+  ZipCode code = 87101;
+  int number = 42;
+
+  print(code is ZipCode);
+  print(code is int);
+  print(number is ZipCode);
+  print(number is int);
+}
+
+void closures() {
+  var counter = 0;
+  final incrementCounter = () {
+    counter += 1;
+  };
+
+  incrementCounter();
+  incrementCounter();
+  incrementCounter();
+  incrementCounter();
+  incrementCounter();
+  print(counter);
+
+  final counter1 = countingFunction();
+  final counter2 = countingFunction();
+  print(counter1());
+  print(counter2());
+  print(counter1());
+  print(counter1());
+  print(counter2());
+}
+
+Function countingFunction() {
+  var counter = 0;
+  final incrementCounter = () {
+    counter += 1;
+    return counter;
+  };
+  return incrementCounter;
+}
