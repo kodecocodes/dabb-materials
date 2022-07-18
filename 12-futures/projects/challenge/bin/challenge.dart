a433ebe95133e4a2c09f2c67ca04825cc4d845ea
suragch <suragch@suragch.dev>
2022-07-18 16:53:30 -0700

    Futures done

--- 12-futures/projects/challenge/bin/challenge.dart
+++ 12-futures/projects/challenge/bin/challenge.dart
@@ -7,9 +7,30 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 
 Future<void> main() async {
+  await exercise();
   await challenge1();
   await challenge2();
   await challenge3();
+}
+
+/// Exercise
+///
+/// 1. Use the `Future.delayed` constructor to provide a string after two
+///    seconds that says, "I am from the future."
+/// 2. Create a `String` variable named `message` that awaits the future to
+///    complete with a value.
+/// 3. Surround the code above with a `try-catch` block.
+Future<void> exercise() async {
+  print('Starting exercise...');
+  try {
+    final message = await Future<String>.delayed(
+      Duration(seconds: 2),
+      () => 'I am from the future.',
+    );
+    print(message);
+  } catch (error) {
+    print(error);
+  }
 }
 
 /// Challenge 1: Spotty Internet
