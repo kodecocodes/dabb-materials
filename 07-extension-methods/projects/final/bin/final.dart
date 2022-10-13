0accea2b69d7cca165881529a7c318e44b658be2
suragch <suragch@suragch.dev>
2022-10-13 13:30:04 +0800

    chapter 7 author final review

--- 07-extension-methods/projects/final/bin/final.dart
+++ 07-extension-methods/projects/final/bin/final.dart
@@ -27,13 +27,8 @@ String encode(String input) {
 }
 
 extension on String {
-  String get encoded {
-    return _code(1);
-  }
-
-  String get decoded {
-    return _code(-1);
-  }
+  String get encoded => _code(1);
+  String get decoded => _code(-1);
 
   String _code(int step) {
     final output = StringBuffer();
